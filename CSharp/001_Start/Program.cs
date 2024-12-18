a426d4f062ca415f7a0ccc5dc27eab4f67c6c3b0
Kim Tae Wook <gmill0411@naver.com>
2024-12-18 10:49:34 +0900

    Start

---
+++ CSharp/001_Start/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// .NET 6부터 새 C# 콘솔 앱용 프로젝트 템플릿은 Program.cs 파일에 다음 코드를 생성
+
+//using System;
+
+//namespace MyApp
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
+//        }
+//    }
+//}
