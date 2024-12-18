e05fb5dcfa4994d905399a98af05700221ee7129
Kim Tae Wook <gmill0411@naver.com>
2024-12-18 13:13:33 +0900

    string method

---
+++ CSharp/008_String/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string s1 = "abcd";
+
+Console.WriteLine(s1.ToUpper());
+Console.WriteLine(s1);
+
+string s2 = "ABCD";
+
+Console.WriteLine(s2.ToLower());
+Console.WriteLine(s2);
+
+string s3 = "name phone_number address,potato,sweet_potato,tomato";
+string[] conditions = s3.Split(new char[] { ' ', ',' });
+
+foreach (string condition in conditions)
+{
+    Console.WriteLine(condition);
+}
+
+string s4 = "\t abc dslkfej\t\t";
+Console.WriteLine("::" + s4 + "::");
+Console.WriteLine("::" + s4.Trim() + "::");
+Console.WriteLine("::" + s4.TrimStart() + "::");
+Console.WriteLine("::" + s4.TrimEnd() + "::");
+
+string[] Menu = { "Pasta", "Pizza", "Steak" };
+string AllMenu = string.Join(", ", Menu);
+Console.WriteLine(AllMenu);
