52c11828e422fe53b5b3d8db0ae7b3891a36974b
Kim Tae Wook <gmill0411@naver.com>
2024-12-19 12:20:37 +0900

    Class

---
+++ CSharp/009_Class/Program.cs
@@ -0,0 +1,21 @@
+﻿using MyClass;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student NewStudent = new Student("Bibi", 18, 'A');
+            StudentManager.GetManager().RegistStudent("Nami", 17, 'B');
+            StudentManager.GetManager().RegistStudent("Zoro", 17, 'B');
+            StudentManager.GetManager().RegistStudent("Rupy", 17, 'B');
+            StudentManager.GetManager().RegistStudent("Robbin", 17, 'B');
+            StudentManager.GetManager().RegistStudent(NewStudent);
+
+            Student findStudent = StudentManager.GetManager().FindStudent("Zoro");
+            findStudent.PrintStudentInfo();
+        }
+    }
+}
---
+++ CSharp/009_Class/Student.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass
+{
+    // [접근 제한자 : default = internal] class [클래스 이름]
+    internal class Student
+    {
+        // 인스턴스 변수
+        // 클래스를 이용해 생성된 인스턴스에서 사용하는 변수
+        // [접근 제한자 : default = private] 자료형 [변수명]
+        private string name;
+        private int age;
+        private char className;
+
+        internal Student(string _name, int _age, char _className)
+        {
+            name = _name;
+            age = _age;
+            className = _className;
+        }
+
+        public void PrintStudentInfo()
+        {
+            Console.WriteLine("name : " + name);
+            Console.WriteLine("age : " + age);
+            Console.WriteLine("className : " + className);
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+    }
+
+    internal class StudentManager
+    {
+        // 클래스 변수
+        // 인스턴스를 생성하지 않고 클래스 명을 이용해 사용할 수 있는 변수
+        // [접근 제한자 : default = private] static 자료형 [변수명]
+        private static StudentManager studentManager = null;
+        private StudentManager() { }
+
+        // 클래스 메서드
+        // 인스턴스를 생성하지 않고 클래스 명을 이용해 사용할 수 있는 메서드
+        public static StudentManager GetManager()
+        {
+            if (studentManager == null)
+            {
+                studentManager = new StudentManager();
+            }
+
+            return studentManager;
+        }
+
+        public void RegistStudent(string _name, int _age, char _className)
+        {
+            Student newStudent = new Student(_name, _age, _className);
+
+            allStudent.Add(newStudent);
+        }
+        public void RegistStudent(Student _student)
+        {
+            allStudent.Add(_student);
+        }
+
+        public Student FindStudent(string _name)
+        {
+            foreach(Student curStudent in allStudent)
+            {
+                if(curStudent.GetName() == _name)
+                {
+                    return curStudent;
+                }
+            }
+
+            return null;
+        }
+
+        private List<Student> allStudent = new List<Student>();
+    }
+}
