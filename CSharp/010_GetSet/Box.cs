65d4f01477c61d80f4bd99e725a627a58536a2ba
Kim Tae Wook <gmill0411@naver.com>
2024-12-19 14:24:59 +0900

    Get/Set

---
+++ CSharp/010_GetSet/Box.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape
+{
+    internal class Circle
+    {
+        private int radius;
+
+        public void SetRadius(int _radius)
+        {
+            if(radius < 0)
+            {
+                Console.WriteLine("반지름 크기는 0보다 큰 수로 지정해주세요");
+            }
+            else
+            {
+                radius = _radius;
+            }
+        }
+
+        public int GetRadius()
+        {
+            return radius;
+        }
+    }
+
+    internal class Box
+    {
+        private int width;
+        public int Width
+        {
+            get {  return width; }
+            set
+            {
+                if(value > 0)
+                {
+                    width = value;
+                }
+                else
+                {
+                    Console.WriteLine("너비는 0보다 큰 수여야 합니다");
+                }
+            }
+        }
+
+        private int height;
+        public int Height
+        {
+            get { return height; }
+            set
+            {
+                if (value > 0)
+                {
+                    height = value;
+                }
+                else
+                {
+                    Console.WriteLine("높이는 0보다 큰 수여야 합니다.");
+                }
+            }
+        }
+    }
+}
---
+++ CSharp/010_GetSet/Program.cs
@@ -0,0 +1,15 @@
+﻿using Shape;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box newBox = new Box();
+            newBox.Width = 100;
+            newBox.Height = 100;
+        }
+    }
+}
