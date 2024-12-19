d23e30affcc166e3ef21098ed15ad2c9e12516fb
Kim Tae Wook <gmill0411@naver.com>
2024-12-19 15:16:16 +0900

    Inherit 1차

---
+++ CSharp/011_Inherit/Animal.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllAnimal
+{
+    internal class Animal
+    {
+        protected string name = "";
+        protected string cryingSound = "";
+
+        public Animal(string _name, string _cryingSound)
+        {
+            name = _name;
+            cryingSound = _cryingSound;
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine(name + "가 밥을 먹습니다.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine(name + "가 잡니다.");
+        }
+
+        public void Cry()
+        {
+            Console.WriteLine(name + "가 " +  cryingSound + "하고 웁니다.");
+        }
+    }
+
+    // 상속을 통해 부모의 public, protected 로 지정된 멤버는 접근 가능
+    // [접근 제한자] class [자식 클래스 이름] : [부모 클래스 이름]
+
+    internal class Dog : Animal
+    {
+        public Dog(string _name = "강아지", string _cryingSound = "왈왈")
+            : base(_name, _cryingSound)
+        {
+
+        }
+
+        public void Bark()
+        {
+            Eat();
+            Cry();
+            Sleep();
+        }
+    }
+
+    internal class Cat : Animal
+    {
+        public Cat(string _name = "고양이", string _cryingSound = "야옹")
+            : base(_name, _cryingSound)
+        {
+
+        }
+
+        public void Meow()
+        {
+            Eat();
+            Cry();
+            Sleep();
+        }
+    }
+}
---
+++ CSharp/011_Inherit/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AllAnimal;
+
+namespace _011_Inherit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            animals.Add(new Dog());
+            animals.Add(new Cat());
+            animals.Add(new Cat("네코", "미야옹"));
+
+            foreach(Animal animal in animals)
+            {
+                if(animal is Dog)
+                {
+                    // 일반적인 자료형 변환
+                    // 변환에 실패하면 예외가 발생
+                    ((Dog)animal).Bark();
+                }
+
+                // as키워드 변환
+                // as 키워드 사용 시 변환할 수 없으면 결과값 null
+                Cat cat = animal as Cat;
+
+                if(cat != null)
+                {
+                    cat.Meow();
+                }
+            }
+        }
+    }
+}
