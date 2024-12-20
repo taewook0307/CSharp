c684682d229a7ef5661a2548467ee9bae1b4d032
Kim Tae Wook <gmill0411@naver.com>
2024-12-20 14:29:29 +0900

    Abstract

---
+++ CSharp/014_Abstract/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _014_Abstract
+{
+    internal abstract class FlyUnit
+    {
+        // abstract 메서드는 abstract 클래스에서만 선언 가능
+        public abstract void Fly();
+    }
+
+    internal class Airplane : FlyUnit
+    {
+        // abstract 메서드는 반드시 재정의 필요
+        public override void Fly()
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // abstract로 선언된 클래스는 인스턴스 생성 불가능
+            // FlyUnit Error = new FlyUnit();
+        }
+    }
+}
