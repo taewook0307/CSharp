6089e81b403630db2b0467a450f9769c61f16b77
Kim Tae Wook <gmill0411@naver.com>
2024-12-20 14:18:58 +0900

    Sealed

---
+++ CSharp/013_Sealed/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _013_Sealed
+{
+    internal class Traffic
+    {
+        public virtual void Run()
+        {
+
+        }
+    }
+
+    internal sealed class Car : Traffic
+    {
+
+    }
+
+    // sealed 키워드를 선언된 클래스는 상속받지 못 함
+    //internal class RacingCar : Car
+    //{
+
+    //}
+
+    internal class Motorcycle : Traffic
+    {
+        public sealed override void Run()
+        {
+
+        }
+    }
+
+    internal class Tricycle : Motorcycle
+    {
+        // sealed 키워드가 선언된 Run메서드는 override 못 함
+        //public override void Run()
+        //{
+
+        //}
+    }
+}
