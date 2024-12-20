e22f53f33ad041179a15c34acc2e7609d5284b08
Kim Tae Wook <gmill0411@naver.com>
2024-12-20 14:09:57 +0900

    Override

---
+++ CSharp/012_Override/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllAnimal
+{
+    internal class Animal
+    {
+        public Animal() { }
+
+        public virtual void Eat()
+        {
+            Console.WriteLine("먹습니다");
+        }
+    }
+
+    internal class Dog : Animal
+    {
+        public Dog()
+            : base()
+        {
+        }
+
+        // 오버라이드
+        public override void Eat()
+        {
+            Console.WriteLine("강아지가 먹습니다.");
+        }
+    }
+
+    internal class Cat : Animal
+    {
+        public Cat()
+            : base()
+        {
+        }
+
+        // 하이딩
+        public new void Eat()
+        {
+            Console.WriteLine("고양이가 먹습니다.");
+        }
+    }
+}
---
+++ CSharp/012_Override/Program.cs
@@ -0,0 +1,16 @@
+﻿using AllAnimal;
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Dog();
+            dog.Eat();
+            Animal cat = new Cat();
+            cat.Eat();
+        }
+    }
+}
