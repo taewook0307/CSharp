0b2252abdb30679d317f0c39959ee6b3cb7c6940
Kim Tae Wook <gmill0411@naver.com>
2024-12-24 11:00:06 +0900

    Tuple

---
+++ CSharp/018_Tuple/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _018_Tuple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tuple<int, string>> tuples = new List<Tuple<int, string>>();
+
+            tuples.Add(new Tuple<int, string>(1, "삼성전자"));
+            tuples.Add(new Tuple<int, string>(2, "현대"));
+            tuples.Add(new Tuple<int, string>(3, "KIA"));
+            tuples.Add(new Tuple<int, string>(4, "LG"));
+            tuples.Add(new Tuple<int, string>(5, "APPLE"));
+
+            foreach (Tuple<int, string> tuple in tuples)
+            {
+                Console.WriteLine($"{tuple.Item1} : {tuple.Item2}");
+            }
+        }
+    }
+}
