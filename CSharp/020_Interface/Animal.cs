567e95f8127d3e03744d491db7c70bbba0cda222
Kim Tae Wook <gmill0411@naver.com>
2024-12-24 12:41:22 +0900

    Interface

---
+++ CSharp/020_Interface/Animal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _020_Interface
+{
+    internal class Animal
+    {
+        virtual public void Eat()
+        {
+            Console.WriteLine("먹습니다");
+        }
+    }
+
+    // 인터페이스는 내부에 메소드, 이벤트, 인덱서, 프로퍼티만 가질 수 있음
+    // 접근 제한자를 사용할 수 없고 모든 것이 public으로 선언됨
+    // 자신을 상속받은 클래스에게 오버라이딩을 강제함
+    // 구현부 없음
+    interface ICanFly
+    {
+        void Fly();
+        void Fly(int _speed);
+    }
+
+    // 클래스는 여러 클래스를 상속받을 수 없지만 여러 인터페이스는 상속 가능
+    internal class Bird : Animal, ICanFly
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("새가 먹습니다.");
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine("새가 납니다.");
+        }
+
+        public void Fly(int _speed)
+        {
+            Console.WriteLine(_speed + "의 속도로 납니다");
+        }
+    }
+}
---
+++ CSharp/020_Interface/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _020_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
