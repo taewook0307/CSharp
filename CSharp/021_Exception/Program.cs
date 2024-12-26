660d3e95d69dc7b79e1c9f18dd96b16bea6b70e1
Kim Tae Wook <gmill0411@naver.com>
2024-12-26 11:22:32 +0900

    Exception

---
+++ CSharp/021_Exception/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _021_Exception
+{
+    internal class Program
+    {
+        public int Value
+        {
+            get; set;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var sw = new StreamWriter("./test.txt"))
+                {
+                    sw.WriteLine("Hello");
+                }
+            }
+            // Put the more specific exceptions first.
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            // Put the least specific exception last.
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine("Done");
+        }
+    }
+}
