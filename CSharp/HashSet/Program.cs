d4af27cc6a9a418b4d43b0669224f241ca3abc57
Kim Tae Wook <gmill0411@naver.com>
2024-12-26 13:19:12 +0900

    HashSet

---
+++ CSharp/HashSet/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 해시 테이블을 이용해 빠른 검색이 가능
+            // 중복 허용 X
+            HashSet<string> strings = new HashSet<string>();
+
+            strings.Add("Rupy");
+            strings.Add("Robbin");
+            strings.Add("Zoro");
+
+            foreach (string s in strings)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
