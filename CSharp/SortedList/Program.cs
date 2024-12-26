59f253ab592c58e2d870200356cd300f98b1baa4
Kim Tae Wook <gmill0411@naver.com>
2024-12-26 12:49:22 +0900

    SortedList 내용 추가

--- CSharp/SortedList/Program.cs
+++ CSharp/SortedList/Program.cs
@@ -6,6 +6,7 @@
         {
             // Key를 기준으로 정렬하여 데이터를 저장하는 구조
             // Key값의 중복 허용 X
+            // Key값과 Value값의 배열을 이용해 데이터를 저장
             SortedList<int, string> students = new SortedList<int, string>();
 
             students.Add(4, "Tearia");
