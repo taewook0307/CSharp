56bbc8a976e5841f9b5a27f5f90f54283acfa72d
Kim Tae Wook <gmill0411@naver.com>
2024-12-26 14:00:13 +0900

    SortedSet

---
+++ CSharp/SortedSet/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 이진트리를 이용하여 데이터를 정렬하여 저장하는 Set
+            SortedSet<int> numbers = new SortedSet<int>();
+
+            numbers.Add(99);
+            numbers.Add(39);
+            numbers.Add(62);
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
