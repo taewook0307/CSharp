4753e6036ff09a4263286b005fe848905e10255b
Kim Tae Wook <gmill0411@naver.com>
2025-01-02 12:09:37 +0900

    string 출력 내용 추가

--- CSharp/008_String/Program.cs
+++ CSharp/008_String/Program.cs
@@ -1,5 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// 출력 방법
+Console.WriteLine("바로 출력");
+
+string str1 = "변수 이용";
+Console.Write(str1);
+
+string str2 = "Format 이용";
+Console.WriteLine("Format 방식 : {0} {1}", str2, 1);
+
+string str3 = "문자열 보간";
+Console.WriteLine($"Format 방식 : {str2} {str3}");
+
+// 대소문자 함수
 string s1 = "abcd";
 
 Console.WriteLine(s1.ToUpper());
@@ -10,6 +23,7 @@ string s2 = "ABCD";
 Console.WriteLine(s2.ToLower());
 Console.WriteLine(s2);
 
+// 문자열 분리 함수
 string s3 = "name phone_number address,potato,sweet_potato,tomato";
 string[] conditions = s3.Split(new char[] { ' ', ',' });
 
@@ -18,6 +32,7 @@ foreach (string condition in conditions)
     Console.WriteLine(condition);
 }
 
+// 공백 제거 함수
 string s4 = "\t abc dslkfej\t\t";
 Console.WriteLine("::" + s4 + "::");
 Console.WriteLine("::" + s4.Trim() + "::");
