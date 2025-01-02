9d34ac4150a1f3b81002fbc54f1c8a843231f5a7
Kim Tae Wook <gmill0411@naver.com>
2025-01-02 15:48:05 +0900

    IEnumerable 1차

---
+++ CSharp/022_IEnumerator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MyApp
+{
+    internal class Program
+    {
+        static IEnumerable<int> GenericTest()
+        {
+            yield return 1;
+            Console.WriteLine("yield return 1 after");
+            yield return 2;
+            Console.WriteLine("yield return 2 after");
+            yield return 3;
+            Console.WriteLine("yield return 3 after");
+            yield return 4;
+            Console.WriteLine("yield return 4 after");
+        }
+    
+        static void Main(string[] args)
+        {
+            foreach (int number in GenericTest())
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
